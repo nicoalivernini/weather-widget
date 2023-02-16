e14be3c2e642896d94fc89bf5d161c6da04ab475
Nicolò A <alivernininicolo@gmail.com>
2023-02-16 12:29:50 +0100

    first implementation carousel

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 let APIKey = 'dddfdf818b35e8a44795a3ce3303374f';
 let cityName = ['London', 'Milan', 'Bangkok', 'Los Angeles', 'Nairobi'];
-let dataCity = [];
+//let dataCity = [];
 
-$(document).ready(
+ /* $(document).ready(
   $.each(cityName, (i, element) => {
     $.ajax(
       {
         url: `https://api.openweathermap.org/data/2.5/weather?q=${element}&appid=${APIKey}&units=metric`,
         method: 'GET',
         success: (data, state) => {
-          dataCity.push(data)
-          console.log(data, state, data.name, dataCity)
+          //dataCity.push(data)
+          $('.weatherWidgetContent').append(
+            `<div class="weatherWidgetCardContainer">
+              <div class="weatherWidgetCardContent">
+                <p>${data.name}</p>
+                <p>${data.main.temp}</p>
+                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png";
+              </div>
+            </div>`)
+          console.log(data)
         },
         error: (error) => {
           alert(`E avvenuto un errore ${error}`)
@@ -18,4 +26,54 @@ $(document).ready(
       }
     )
   })
-)
+) */
+
+/* function forecast() {
+  $.ajax({
+    url: `https://api.openweathermap.org/data/2.5/forecast?lat=41.53&lon=12.32&appid=dddfdf818b35e8a44795a3ce3303374f`,
+    method: 'GET',
+    success: (data, state) => {
+      console.log(data)
+    }
+  })
+}  */
+
+  let touchstartX = 0;
+  let touchendX = 0;
+  let gesuredZone = '';
+
+  $(document).ready(() => {
+    let gesuredZone = document.getElementById('ok');
+
+    gesuredZone.addEventListener('touchstart', function(event) {
+      touchstartX = event.changedTouches[0].screenX;
+  }, true);
+
+  gesuredZone.addEventListener('touchend', function(event) {
+      touchendX = event.changedTouches[0].screenX;
+      checkTypeSwipe();
+  }, true); 
+
+  function checkTypeSwipe() {
+    let swipeDirection = '';
+    let counterSlide = 1;
+    if (touchendX < touchstartX) {
+      swipeDirection = 'left'
+    } else if (touchendX > touchstartX) {
+      swipeDirection = 'right'
+    }
+    if(swipeDirection === 'left') {
+      console.log(swipeDirection, 'if');
+      $('.active').addClass('next');
+      //counterSlide+
+      console.log(counterSlide)
+    } else if (swipeDirection === 'right') {
+      console.log(swipeDirection, 'if');
+      $('.active').removeClass('next')
+      //counterSlide-
+      console.log(counterSlide)
+    }
+  } 
+})
+
+  
